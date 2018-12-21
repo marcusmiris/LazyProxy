d46eec89335da0d39366c8b1bfdee04c31913e90
Marcus Miris <marcusmiris@reinodedeus.org>
2018-12-21 18:02:08 -0200

    first version.

---
+++ Miris.LazyProxy/LazyInterceptor`1.cs
@@ -0,0 +1,25 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Miris.LazyProxy
+{
+    public class LazyInterceptor<T>
+        : IInterceptor
+    {
+        private readonly Lazy<Lazy<T>> _lazy;
+
+        public LazyInterceptor(Func<Lazy<T>> lazyFactory)
+        {
+            _lazy = new Lazy<Lazy<T>>(lazyFactory);
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            var target = _lazy.Value.Value;
+
+            invocation.ReturnValue = invocation.GetConcreteMethod().Invoke(
+                target,
+                invocation.Arguments);
+        }
+    }
+}
---
+++ Miris.LazyProxy/LazyProxyBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Castle.DynamicProxy;
+
+namespace Miris.LazyProxy
+{
+    public static class LazyProxyBuilder
+    {
+        public static T CreateProxyForLazy<T>(
+            Func<Lazy<T>> lazyFactory)
+            where T : class
+            => CreateProxyForLazy(lazyFactory, new ProxyGenerator());
+
+
+        public static T CreateProxyForLazy<T>(
+            Func<Lazy<T>> lazyFactory,
+            IProxyGenerator proxyGenerator)
+            where T : class
+        {
+            return proxyGenerator.CreateInterfaceProxyWithoutTarget<T>(
+                new LazyInterceptor<T>(lazyFactory));
+        }
+    }
+}
